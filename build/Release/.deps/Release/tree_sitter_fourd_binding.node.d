41df80984f17fe4aef798ee0efa01b215983a76e
Keisuke Miyako <keisuke.miyako@4d.com>
2020-01-18 01:00:07 +0900

    remove

--- build/Release/.deps/Release/tree_sitter_fourd_binding.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/tree_sitter_fourd_binding.node := c++ -bundle -undefined dynamic_lookup -Wl,-no_pie -Wl,-search_paths_first -mmacosx-version-min=10.10 -arch x86_64 -L./Release -stdlib=libc++  -o Release/tree_sitter_fourd_binding.node Release/obj.target/tree_sitter_fourd_binding/src/parser.o Release/obj.target/tree_sitter_fourd_binding/src/binding.o 
